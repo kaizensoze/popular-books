24439349e92d19ae5c728ed33550a1ad13505709
Joe Gallo <joe@brassafrax.com>
2016-08-09 23:29:04 -0400

    parse aws info from gitignored credentials file
    resolves #2

--- popbooks.py
+++ popbooks.py
@@ -2,17 +2,27 @@
 import base64
 import hashlib
 import hmac
+import json
 import urllib.parse
 
-secret_key = b'1234567890'
+# get aws credentials
+with open('credentials.json') as credentials_file:
+    credentials = json.load(credentials_file)
 
-f = open('test_request.txt', 'rb')
-try:
-    body = f.read()
-finally:
-    f.close()
+if not credentials:
+    raise RuntimeError('Cannot find credentials.json')
 
-digest = hmac.new(secret_key, body, hashlib.sha256).digest()
+AWS_ACCESS_KEY = credentials['aws_access_key_id']
+AWS_SECRET_ACCESS_KEY = credentials['aws_secret_access_key']
+
+# generate Signature param for test request
+with open('test_request.txt', 'rb') as test_request_file:
+    test_request_contents = test_request_file.read()
+
+if not test_request_contents:
+    raise RuntimeError('Cannot find test_request.txt')
+
+digest = hmac.new(str.encode('1234567890'), msg=test_request_contents, digestmod=hashlib.sha256).digest()
 decoded_digest = base64.b64encode(digest).decode()
 urlencoded_decoded_digest = urllib.parse.quote_plus(decoded_digest)
 print(urlencoded_decoded_digest)
